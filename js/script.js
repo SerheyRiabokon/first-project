12b7da2e6f2a85dd5dc4bfade247f3cc062aa275
Sergey Ryabokon <ryabokonsergo@gmail.com>
2020-07-14 20:02:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,112 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    // Tabs
+    
+	let tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
+
+	function hideTabContent() {
+        
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+	}
+
+	function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+    
+    hideTabContent();
+    showTabContent();
+
+	tabsParent.addEventListener('click', function(event) {
+		const target = event.target;
+		if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+		}
+    });
+    //Timer
+    const deadline = '2020-07-18';
+     
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              d = Math.floor(t / (1000 * 60 * 60 * 24)),
+              h = Math.floor((t / 1000 * 3600) % 24),
+              m = Math.floor ((t / 1000 / 60) % 60),
+              s = Math.floor ((t / 1000) % 60);
+
+        return{
+            'total': t,
+            'days': d,
+            'hours': h,
+            'minutes': m,
+            'seconds': s
+        };
+
+    }
+    
+    function getZero(num){
+        if(num > 0 && num < 10){
+            return `0${num}`;
+        }
+        else{
+            return num;
+        }
+    }
+    function setClock(selrctor, endtime){
+        const timer = document.querySelector('.timer'),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+        
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0){
+                clearInterval(timeInterval);
+            }
+
+        }      
+    }
+    setClock('.timer', deadline);
+
+//Modal
+
+    const modalTriger = document.querySelector('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+    console.log(modalTriger);
+    modalTriger.addEventListener('click',() =>{
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+    });   
+    modalCloseBtn.addEventListener('click',() =>{
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    });       
+    
+});
